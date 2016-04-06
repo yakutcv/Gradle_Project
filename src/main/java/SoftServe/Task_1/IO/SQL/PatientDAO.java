093500997407741e815eb5661ab427c6f1fbec07
yakutcv <yakutcv@gmail.com>
2016-04-06 20:12:51 +0300

    Initial commit

---
+++ src/main/java/SoftServe/Task_1/IO/SQL/PatientDAO.java
@@ -0,0 +1,286 @@
+package SoftServe.Task_1.IO.SQL;
+
+import SoftServe.Task_1.Entity.Patient;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by ayasintc on 4/5/2016.
+ */
+public class PatientDAO {
+
+    SQLConnector connector = new SQLConnector();
+    private static Statement statement;
+    private static PreparedStatement preparedStatement;
+    private static ResultSet resultSet;
+    private String TABLE_NAME = "Patients";
+    private String CREATE_PATIENT_TABLE = "CREATE TABLE  "+ TABLE_NAME + " (id int NOT NULL AUTO_INCREMENT, Name text, Last_name text, Birth_date text, PRIMARY KEY (id))";
+    private String DELETE_PATIENT_TABLE = "DROP TABLE " + TABLE_NAME;
+    private String CREATE_PATIENT = "INSERT INTO " + TABLE_NAME + " (Name, Last_name, Birth_date) VALUES (?, ?, ?)";
+    private String DELETE_PATIENT = "DELETE FROM " + TABLE_NAME + " WHERE id =?";
+    private String UPDATE_PATIENT = "UPDATE " + TABLE_NAME + " Patients SET Name =?, Last_name=?, Birth_date=? WHERE id =?";
+    private String READ_PATIENT_BY_ID = "SELECT * FROM "+ TABLE_NAME +" WHERE id =?";
+
+    public boolean createPatientTable() {
+        try{
+            connector.connect();
+            statement = connector.getConnection().createStatement();
+            statement.executeUpdate(CREATE_PATIENT_TABLE);
+            System.out.println("Patients table created!");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't create table Patient!");
+        }
+        catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }finally {
+            try {
+                statement.close();
+                connector.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean deletePatientTable() {
+        try{
+            connector.connect();
+            statement = connector.getConnection().createStatement();
+            statement.executeUpdate(DELETE_PATIENT_TABLE);
+            System.out.println("Patients table delete!");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Table " +  TABLE_NAME + " don't found!");
+        }
+        catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }finally {
+            try {
+                statement.close();
+                connector.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean addPatient(Patient patient){
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(CREATE_PATIENT);
+            preparedStatement.setString(1,patient.getName());
+            preparedStatement.setString(2,patient.getLastName());
+            preparedStatement.setString(3,patient.getBirthDateInString());
+            preparedStatement.executeUpdate();
+            System.out.println("Patients " + patient.getFullName() + "has been added into table " + TABLE_NAME);
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't add Patient into table!");
+        } catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }finally {
+            try {
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean deletePatientById(long id) {
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(DELETE_PATIENT);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("Patient with id " + id + " was deleted!");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't find patient id: " + id);
+        } catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }finally {
+            try {
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean updatePatient (Patient patient){
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(UPDATE_PATIENT);
+            preparedStatement.setString(1, patient.getName());
+            preparedStatement.setString(2, patient.getLastName());
+            preparedStatement.setString(3,patient.getBirthDateInString());
+            preparedStatement.setLong(4, patient.getId());
+            preparedStatement.executeUpdate();
+            System.out.println("Patient with id " + patient.getFullName() + " was deleted!");
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't update patient " + patient);
+        } catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }finally {
+            try {
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean addListPatients(Set<Patient> patients) {
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(CREATE_PATIENT);
+            for(Patient p : patients) {
+                preparedStatement.setString(1, p.getName());
+                preparedStatement.setString(2, p.getLastName());
+                preparedStatement.setString(3, p.getBirthDateInString());
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
+            System.out.println("List patient was add!");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't add List Patients to table " + TABLE_NAME);
+        } catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }finally {
+            try {
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public Patient readPatientById(long id) {
+        Patient patient = null;
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(READ_PATIENT_BY_ID);
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                patient = Patient.newPatientBuilder()
+                        .setId(resultSet.getInt(1))
+                        .setName(resultSet.getString(2))
+                        .setLastName(resultSet.getString(3))
+                        .setBirthDate(resultSet.getString(4))
+                        .build();
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't find patient with id: " + id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                resultSet.close();
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                System.out.println("Connection failed!");
+                e.printStackTrace();
+            }
+        }
+        return patient;
+    }
+
+    public Set<Patient> readAllPatientsFromDB() {
+        String READ_ALL_PATIENTS = "SELECT * FROM Patients";
+        Set<Patient> patients = new HashSet<>();
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(READ_ALL_PATIENTS);
+            resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                patients.add(new Patient().newPatientBuilder()
+                        .setId(resultSet.getInt(1))
+                        .setName(resultSet.getString(2))
+                        .setLastName(resultSet.getString(3))
+                        .setBirthDate(resultSet.getString(4))
+                        .build());
+
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't read patients from table " + TABLE_NAME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                resultSet.close();
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                System.out.println("Connection failed!");
+                e.printStackTrace();
+            }
+        }
+        return patients;
+    }
+
+    public Patient readPatientByIdWithAlalyzes(Patient patient) {
+        Patient pat = null;
+        try{
+            connector.connect();
+            preparedStatement = connector.getConnection().prepareStatement(READ_PATIENT_BY_ID);
+            preparedStatement.setLong(1, patient.getId());
+            resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                pat = Patient.newPatientBuilder()
+                        .setId(resultSet.getInt(1))
+                        .setName(resultSet.getString(2))
+                        .setLastName(resultSet.getString(3))
+                        .setBirthDate(resultSet.getString(4))
+                        .build();
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't find patient " + patient.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                resultSet.close();
+                preparedStatement.close();
+                connector.close();
+            } catch (SQLException e) {
+                System.out.println("Connection failed!");
+                e.printStackTrace();
+            }
+        }
+        return pat;
+    }
+
+
+}
---
+++ src/main/java/SoftServe/Task_1/Test/TestMain.java
@@ -0,0 +1,192 @@
+package SoftServe.Task_1.Test;
+
+/**
+ * Created by ayasintc on 3/29/2016.
+ */
+
+import SoftServe.Task_1.Entity.Analysis;
+import SoftServe.Task_1.Entity.Patient;
+import SoftServe.Task_1.IO.SQL.AnalyzesDAO;
+import SoftServe.Task_1.IO.SQL.PatientDAO;
+import SoftServe.Task_1.Logic.Hospital;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static SoftServe.Task_1.Entity.AnalysisType.*;
+
+public class TestMain {
+
+    public static void main(String[] args) throws Exception {
+
+        Patient first = Patient.newPatientBuilder()
+                .setBirthDate("04/04/1987")
+                .setName("Andrew")
+                .setLastName("Yasinskiy")
+                .setId(1)
+                .setAnalysis(Analysis.newAnalysisBuilder()
+                        .setId(1)
+                        .setType(HORMONES)
+                        .setDate("03/02/2015 14:50")
+                        .setReport("I don't know what is is...")
+                        .build())
+                .setAnalysis(Analysis.newAnalysisBuilder()
+                        .setId(2)
+                        .setType(ALLERGY)
+                        .setDate("03/02/2015 14:15")
+                        .setReport("Good")
+                        .build())
+                .build();
+
+
+        Patient second = Patient.newPatientBuilder()
+                .setBirthDate("04/04/1954")
+                .setName("Petia")
+                .setLastName("Petrushkin")
+                .setId(3)
+                .setAnalysis(Analysis.newAnalysisBuilder()
+                        .setId(1)
+                        .setType(BLOOD)
+                        .setDate("03/01/2014 14:15")
+                        .setReport("Yeap")
+                        .build())
+                .setAnalysis(Analysis.newAnalysisBuilder()
+                        .setId(2)
+                        .setType(ALLERGY)
+                        .setDate("03/01/2016 14:15")
+                        .setReport("Mu-ha-ha")
+                        .build())
+                .build();
+
+
+        Patient third = Patient.newPatientBuilder()
+                .setBirthDate("04/04/1998")
+                .setName("Jora")
+                .setLastName("Jorkin")
+                .setId(4)
+                .setAnalysis(Analysis.newAnalysisBuilder()
+                        .setId(1)
+                        .setType(ALLERGY)
+                        .setDate("02/01/2016 14:40")
+                        .setReport("Good Analysis.Very good!")
+                        .build())
+                .setAnalysis(Analysis.newAnalysisBuilder()
+                        .setId(2)
+                        .setType(BLOOD)
+                        .setDate("02/01/2016 14:44")
+                        .setReport("Simple good")
+                        .build())
+                .build();
+
+        Hospital hospital = new Hospital();
+        hospital.addPatient(first);
+        hospital.addPatient(second);
+        hospital.addPatient(third);
+
+        Analysis analysis_1 = Analysis.newAnalysisBuilder()
+                .setId(2)
+                .setType(ALLERGY)
+                .setDate("02/01/2016 13:10")
+                .setReport("Analysis fdf")
+                .build();
+
+        Analysis analysis_2 = Analysis.newAnalysisBuilder()
+                .setType(BIOPSY)
+                .setDate("04/02/2016 13:10")
+                .setReport("GOOD")
+                .build();
+
+        Analysis analysis_3 = Analysis.newAnalysisBuilder()
+                .setType(RH_FACTOR)
+                .setDate("04/03/2016 13:10")
+                .setReport("sdgfsdgsdgsdg")
+                .build();
+
+        List<Analysis> listAnalysis = new ArrayList<>();
+        listAnalysis.add(analysis_1);
+        listAnalysis.add(analysis_2);
+        listAnalysis.add(analysis_3);
+
+
+        //System.out.println(third.getBirthDateInString());
+
+        /*JSONIO js = new JSONIO();
+        js.writeHospital(hospital, "myJson");
+        js.readHospital("myJson");
+        js.readHospital("myJson");*//*
+
+
+        //XMLIO xmlIO = new XMLIO();
+        //xmlIO.writeHospital(hospital, "hospital.xml");
+
+        //Hospital hospitalSecond =  xmlIO.readHospital("hospital.xml");
+        //System.out.println(hospitalSecond.getByAnalisisType(BLOOD));*/
+
+        //SelfFormatIO self = new SelfFormatIO();
+        //self.writeHospital(hospital,"myTxt.txt");
+
+        //Hospital hosp2 = self.readHospital("myTxt.txt");
+        //System.out.println(hosp2);
+        PatientDAO ptDAO = new PatientDAO();
+        //ptDAO.addPatient(second);
+        //ptDAO.addPatient(third);
+        //ptDAO.deletePatientTable();
+        //ptDAO.removePatientTable();
+        //ptDAO.createPatientTable();
+        //ptDAO.addPatient(first);
+        //ptDAO.addListPatients(hosp2.getPatients());
+        Hospital hospital1 = new Hospital();
+        //hospital1.setPatients(ptDAO.readAllPatients());
+        //ptDAO.updatePatient(third);
+        //ptDAO.createPatientTable();
+
+        //ptDAO.addPatient(first);
+        //ptDAO.deletePatientById(2);
+
+        AnalyzesDAO ADAO = new AnalyzesDAO();
+        //ADAO.deletePatientTable();
+        //ADAO.createAnalyzesTable();
+        //ADAO.addAnalysis(analysis_1, first);
+        //ADAO.addListAnalysis(listAnalysis, second);
+        //ADAO.deletePatientTable();
+        //ADAO.updateAnalysis(analysis_1);
+        //ADAO.deleteAnalysisById(1);
+
+        //System.out.println(ptDAO.readPatientByIdWithAlalyzes(first));
+
+
+        //Analysis dsdf = ADAO.readAnalyzesById(14);
+        List<Analysis> sdf = ADAO.readAllAnalyzesByPatient(second);
+        System.out.println(sdf);
+
+
+
+
+
+        //Hospital hospital14 = self.readHospital("myTxt.txt");
+        //System.out.println(hospital14);
+
+        //System.out.println(String.valueOf(first.getId()));
+
+        //System.out.println(self.readHospital("myTxt.txt"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
